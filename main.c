e285e0f7b202e2d05228203350ea2917bf02c328
César Chávez <79390377+CsarChvz@users.noreply.github.com>
2025-03-12 10:07:04 -0600

    laaaa

--- main.c
+++ main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "logo.h"
+#include "common/logo.h"
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	printf("Hola");
+	
 	imprimir_logo();  
 	return 0;
 }
