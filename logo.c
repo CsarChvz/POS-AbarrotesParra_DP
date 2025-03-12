9c2a897f611c32684dc850eaea6dafc2614057a2
César Chávez <79390377+CsarChvz@users.noreply.github.com>
2025-03-12 09:58:11 -0600

    Logo

---
+++ logo.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void imprimir_logo() {
+    printf("           ,ggg,                                                                                   \n");
+    printf("          dP\"\"8I   ,dPYb,                                                  I8                      \n");
+    printf("         dP   88   IP'`Yb                                                  I8                      \n");
+    printf("        dP    88   I8  8I                                               88888888                   \n");
+    printf("       ,8'    88   I8  8'                                                  I8                      \n");
+    printf("       d88888888   I8 dP       ,gggg,gg   ,gggggg,   ,gggggg,    ,ggggg,   I8    ,ggg,     ,g,    \n");
+    printf(" __   ,8\"     88   I8dP   88ggdP\"  \"Y8I   dP\"\"\"8I   dP\"\"\"8I   dP\"  \"Y8gggI8   i8\" \"8i   ,8'8,   \n");
+    printf("dP\"  ,8P      Y8   I8P    8I i8'    ,8I  ,8'    8I  ,8'    8I  i8'    ,8I ,I8,  I8, ,8I  ,8'  Yb  \n");
+    printf("Yb,_,dP       `8b,,d8b,  ,8I,d8,   ,d8b,,dP     Y8,,dP     Y8,,d8,   ,d8',d88b, `YbadP' ,8'_   8) \n");
+    printf(" \"Y8P\"         `Y88P'\"Y88P\"'P\"Y8888P\"`Y88P      `Y88P      `Y8P\"Y8888P\"  8P\"\"Y8888P\"Y888P' \"YY8P8P\n");
+    printf("                                                                                                  \n");
+    printf("                                                                                                  \n");
+    printf(" ,ggggggggggg,                                                                                     \n");
+    printf("dP\"\"\"88\"\"\"\"Y8,                                                                                   \n");
+    printf("Yb,  88      `8b                                                                                   \n");
+    printf(" \"`  88      ,8P                                                                                   \n");
+    printf("     88aaaad8P\"                                                                                    \n");
+    printf("     88\"\"\"\",gggg,gg   ,gggggg,   ,gggggg,    ,gggg,gg                                             \n");
+    printf("     88    dP\"  \"Y8I   dP\"\"\"8I   dP\"\"\"8I   dP\"  \"Y8I                                             \n");
+    printf("     88   i8'    ,8I  ,8'    8I  ,8'    8I  i8'    ,8I                                             \n");
+    printf("     88  ,d8,   ,d8b,,dP     Y8,,dP     Y8,,d8,   ,d8b,                                            \n");
+    printf("     88  P\"Y8888P\"`Y88P      `Y88P      `Y8P\"Y8888P\"`Y8                                            \n");
+    printf("                                                                                                  \n");
+    printf("                                                                                                  \n");
+    printf("                                                                                                  \n");
+    printf("                                                                                                  \n");
+}
