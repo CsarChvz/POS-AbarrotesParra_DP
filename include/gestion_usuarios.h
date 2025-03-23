2b5363d8d3c2377d15b4488c288fd5b38221668f
César Chávez <79390377+CsarChvz@users.noreply.github.com>
2025-03-23 17:56:59 -0600

    Archivo transacciones.csv. Definiendo el formato

--- include/gestion_usuarios.h
+++ include/gestion_usuarios.h
@@ -9,13 +9,6 @@ typedef struct {
     int edad;
 } Usuario;
 
-typedef struct {
-    int id;
-    int usuario_id;
-    char descripcion[100];
-    float monto;
-    char fecha[11]; // formato: YYYY-MM-DD
-} Transaccion;
 
 int obtenerUsuarios(Usuario usuarios[], int max_usuarios);
 
---
+++ include/transacciones.h
@@ -0,0 +1,19 @@
+#ifndef TRANSACCIONES_H
+#define TRANSACCIONES_H
+
+typedef struct {
+    int idTransaccion;
+    char fecha[11]; // YYYY-MM-DD
+    char tipoMovimiento[10]; // Ingreso o Egreso
+    char categoriaTransaccion[20]; // Venta, Compra, etc.
+    float ingreso;
+    float egreso;
+    float saldo;
+    char metodoPago[20];
+    int idUsuario;
+    char observaciones[100];
+    char corteCaja[4]; // "Sí" o "No"
+    char inicioJornada[4]; // "Sí" o "No"
+} Transaccion;
+
+#endif
